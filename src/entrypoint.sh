5aac110152263d44fa1d8724cd16f3c224943356
Stephan Wendel <me@stephanwe.de>
2022-09-17 08:21:13 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,11 +5,20 @@
 
 set -eoux pipefail
 
-echo $SHELL
-echo "Hello! I am working :)"
+# source functions.sh
+if [[ -f "/functions.sh" ]]; then
+    source /functions.sh
+else
+    echo -e "functions.sh not found! Exiting..."
+    exit 1
+fi
+
 
 # Check git command
 if [[ -z "$(command -v git)" ]]; then
     echo "Command 'git' not found."
     exit 1
 fi
+
+
+
---
+++ src/functions.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# written by Stephan Wendel aka KwadFan <me@stephanwe.de>
+# Copyright 2022
+# This file is distributed under GPLv3
+
+# This file contains fuctions needed by entypoint.sh
